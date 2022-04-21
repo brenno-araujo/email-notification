ace30a0f8d1af5ef068b7f58cad023b4498103f3
Brenno Araújo <brennoaraujo@yahoo.com>
2022-04-20 23:19:17 -0300

    Feat: emailNotification

---
+++ app/Notifications/UserNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserNotification extends Notification
+{
+    use Queueable;
+
+    private $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Esta é a confirmação de cadastro de usuário.')
+                    ->greeting('Olá'.$this->user->name)
+                    ->action('Clique aqui para confirmar o seu cadastro', url('/'))
+                    ->line('Se você não solicitou este cadastro, nenhuma ação é necessária.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
